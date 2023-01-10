052ed3bcef01b4f9ef35b25158e58157d1dcb0c4
kinjal patel <kinjalpatel@kinjals-MacBook-Air.local>
2023-01-10 16:34:03 -0500

    edited JS Objects Arrays

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-console.log("Hello World!\n==========\n");
-
-// Exercise 1 Section
-
+// Exercise 1
+ 
 const numbers = [2, 22, 12, 17, 18, 39, 129];
-
 function arraySum(arr) {
     console.log(arr);
-    let sum = 0
-    
-    for (let i = 0; i < arr.lengt; i++) {
-        sum = sum + arr[i];
-    }  
-       
-     return sum;
-}
+    let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+   sum += arr[i];
+   }
+   return sum;
+ }
 
 console.log(arraySum(numbers));
 
 
-
-// Exercise 2 Section
+// Exercise 2
 
 let favBook = {};
 
@@ -29,37 +24,36 @@ favBook.pageCount = 295;
 favBook.readCount = 3;
 
 console.log(favBook);
-
 favBook.info = function () {
-    return `${this.title} by ${this.author} is ${this.pageCount} page(s) and has been read ${this.readCount} time(s).`;
+    return `${this.title} by ${this.author} is ${this.pageCount} pages and has been raed ${this.readCount} times.`;
 };
-console.log(favBook.info()); 
-
+console.log(favBook.info());
 
 // Exercise 3
 
 let sentence = "The quick brown fox jumps over the lazy dog";
 
-const words = sentence.split(' ');
+let words = sentence.split(" ");
 console.log(words);
-let Result = [];
+let result = [];
 
-for (let i = 0; i < words.length; i++){
-    let letters = words[i].split("");
-       console.log(letters.slice());
+for (let i = 0; i < words.length; i++) {
+   let letters = words[i].split("");
+   console.log(letters);
 
-    const lettersReversed = letters.reverse();
-       console.log("reversed latters;", letters);
+   let lettersReversed = letters.reverse();
+   console.log("reversed letters:", letters);
 
-    const lettersJoined = lettersReversed.join("")
-       console.log(lettersJoined);
+   let lettersJoined = lettersReversed.join("")
+   console.log(lettersJoined);
 
-    Result.push(lettersJoined);
-
+   result.push(lettersJoined);
+  
 }
 
-const finalsentence = Result.join(" ");
-console.log(finalsentence);
+let finalSentence = result.join(" ");
+
+console.log(finalSentence);
 
 
 // Exercise 4
@@ -67,23 +61,29 @@ console.log(finalsentence);
 let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
 console.log(csvData);
 
-  
-const rows = csvData.split("/n");
+const rows = csvData.split("\n");
 console.log(rows);
 
-const headers = rows[0].split(",");
-console.log(headers);
+const titles = rows[0].split(",");
+console.log(titles);
 const resultArray = [];
 
 for (let i = 1; i < rows.length; i++) {
-    const valueArray = rows[i].split(",");
-    console.log(valueArray);
+    const dataArray = rows[i].split(",");
+    console.log(dataArray);
 
     const dataObject = {
-        name: valueArray[0],
-        age: valueArray[1],   
+        name: dataArray[0],
+        age: dataArray[1],
     };
-
-    resultArray.push(valueObject);
+    resultArray.push(dataObject);
 }
+
+
+
+
+
+
+
+
 
